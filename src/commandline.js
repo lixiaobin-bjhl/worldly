c15ad9ba3a9f9e6971c5c0cbda31ef4ea4629703
Peter Wang <happypeter1983@gmail.com>
2013-03-23 10:00:22 +0800

    发布0.04版本到npm

---
+++ src/commandline.js
@@ -0,0 +1,67 @@
+/*
+ * 命令行UI程序
+ * 依赖shell
+ */
+var shell = require('../lib/shell.js');
+var fs = require('fs');
+//由app.js传进来的worldly实例
+var worldly;
+
+//这个模块的初始化
+//参数是worldly模块的实例
+exports.init = function(d) {
+	worldly = d;
+	shell.start(); //准备就绪 开启shell交互
+};
+// 查看
+shell.command('ls', 'less watching files', function(args, next) {
+	for (var i in watchFiles){
+		shell.print('watching  ' + watchFiles[i]);
+	}
+	next();
+});
+
+shell.command('exit', 'exit wroldly', function(args, next) {
+	shell.print('Bye! ^_^');
+	process.exit();
+});
+
+shell.command('ver', 'less worldly version', function(args, next) {
+	fs.readFile(paths.base + '/package.json', 'utf-8', function(err, data) {
+		if (err) throw err;
+		var r = JSON.parse(data);
+		shell.print('version：' + r.version);
+		next();
+	});
+});
+
+// 查看配置文件路径
+shell.command('V', 'less config.json path', function(args, next) {
+	shell.print('configFilePath: ' + paths.conf + '/' + 'config.json');
+	next();
+});
+
+// 查看配置文件路径
+shell.command('help', 'less help info', function(args, next) {
+	var c = shell.commands_list,
+	s;
+	for (var item in c) {
+		s = getCommandSpace(c[item].cname);
+		shell.print('  ' + c[item].cname + s + c[item].discribe);
+	}
+	next();
+});
+
+/**
+ *为了command help 信息打印整齐，计算cname后面打空格的个数
+ **/
+function getCommandSpace(cname) {
+	var l = 15 - cname.length,
+	s = '';
+
+	for (var i = 0; i < l; i++) {
+		s += ' ';
+	}
+	return s;
+}
+
--- src/worldly.js
+++ src/worldly.js
@@ -1,32 +1,41 @@
 /**
  * combine and minijs 
- * @fineIn{string or array} readFilePath
- * @fileOut(string) writeFilePath
+ * @param fineIn{string or array} readFilePath
+ * @param fileOut(string) writeFilePath
  * */
 var fs = require('fs');
 var jsp = require("uglify-js").parser;
 var pro = require("uglify-js").uglify;
 var cleanCSS = require('clean-css');
 var path = require('path');
+var coli = require('./commandline.js');
+var UglifyJS = require("uglify-js");
 
 //准备一些常用路径
 var dir_base = path.resolve(__dirname + '/../'); //程序根目录
-var paths = {
+global.paths = {
 	base: dir_base,
 	conf: path.resolve(dir_base + '/conf'),
 };
 global.watchFiles = [];
 /*
  * css 输出函数
- * @fileIn{array} 输出源
- * @fileOut{string}输出target
+ * @param fileIn{array} 输出源
+ * @param fileOut{string}输出target
  * */
 function cssMinifier(flieIn, fileOut) {
-	var files = Array.isArray(flieIn) ? flieIn: [flieIn];
+	var files = Array.isArray(flieIn) ? flieIn: [flieIn],
+	origCode,
+	finalCode = '',
+	p;
 	pushToWatchFile(files);
-	var origCode, finalCode = '';
 	for (var i = 0; i < files.length; i++) {
-		origCode = fs.readFileSync(config.workPath +  files[i], 'utf8');
+		p = path.resolve(config.workPath + files[i]);
+		if (!fs.existsSync(p)) {
+			console.error('cannot find file ' + p);
+			return;
+		}
+		origCode = fs.readFileSync(p, 'utf8');
 		finalCode += cleanCSS.process(origCode);
 	}
 	fs.writeFileSync(config.buildPath + fileOut, finalCode, 'utf8');
@@ -34,33 +43,63 @@ function cssMinifier(flieIn, fileOut) {
 
 /*
  * js 输出函数
- * @fileIn{array} 输出源
- * @fileOut{string}输出target
+ * @param fileIn{array} 输出源
+ * @param fileOut{string}输出target
  * */
 function jsMinifier(fileIn, fileOut) {
-	var files = Array.isArray(fileIn) ? fileIn: [fileIn];
+	var files = Array.isArray(fileIn) ? fileIn: [fileIn],
+	p,
+	length = files.length,
+	l = [];
 	pushToWatchFile(files);
-	var origCode, ast, finalCode = '';
-	for (var i = 0; i < files.length; i++) {
-		origCode = fs.readFileSync(config.workPath + files[i], 'utf8');
-		ast = jsp.parse(origCode);
-		ast = pro.ast_mangle(ast);
-		ast = pro.ast_squeeze(ast);
-		finalCode += ';' + pro.gen_code(ast);
+	for (var i = 0; i < length; i++) {
+		p = path.resolve(config.workPath + files[i]);
+		if (!fs.existsSync(p)) {
+			console.error('cannot find file ' + p);
+			return;
+		}
+		l.push(p);
+		//origCode = fs.readFileSync(p, 'utf8');
+		//ast = jsp.parse(origCode);
+		//ast = pro.ast_mangle(ast);
+		//ast = pro.ast_squeeze(ast);
+		//finalCode += ';' + pro.gen_code(ast);
 	}
-	fs.writeFileSync(config.buildPath + fileOut, finalCode, 'utf8');
+	//fs.writeFileSync(config.buildPath + fileOut, finalCode, 'utf8');
+	UglifyJS.minify(l, {
+		outSourceMap: config.buildPath + fileOut
+	});
+}
+
+/*
+ * 创建文件夹
+ * param path{string} 需要创建文件夹的路径
+ **/
+function createDir(p) {
+	var p = path.resolve(p);
+	if (!fs.existsSync(p)) {
+		try {
+			fs.mkdirSync(p, 0777);
+			console.log('mkdir: ' + p);
+		} catch(e) {
+			console.log(e.stack);
+		}
+	}
 }
 
 fs.readFile(paths.conf + '/config.json', 'utf-8', function(err, data) {
 	if (err) throw err;
 	global.config = JSON.parse(data);
+	createDir(config.buildPath);
+	createDir(config.workPath);
 	for (var item in config.combinJS) {
 		jsMinifier(config.combinJS[item].fileIn, config.combinJS[item].fileOut);
 	}
-
 	for (var item in config.combinCSS) {
 		cssMinifier(config.combinCSS[item].fileIn, config.combinCSS[item].fileOut);
 	}
+	console.log('worldly ready！configFilePath：' + paths.conf + '/config.json');
+	coli.init();
 });
 
 /**
@@ -138,20 +177,7 @@ function pushToWatchFile(files) {
 	}
 }
 
-process.on('exit', function() {
-	console.log('exit lalala~~~');
-});
-
 process.on('uncaughtException', function(err) {
 	console.log(err.stack);
-});
-
-// 检测信号
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', function(chunk) {
-	if (chunk.trim() == 'watch') {
-		console.log('watching file', watchFiles);
-	}
 });
 
